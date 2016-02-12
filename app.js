55f6707ab5325c991af0cc2cc402609a5055653f
Lauxx <lm.nichols@hotmail.com>
2016-02-12 11:24:06 -0700

    manipulated the list in a forEach loop to append current user information

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-var personOne = {firstName: "john", lastName: "johnson", phoneNumber: '404-333-3232'};
-var personTwo = {firstName: "sally", lastName: "johnson", phoneNumber: '404-334-2323'};
-var personThree = {firstName: "sue", lastName: "johnson", phoneNumber: '404-335-3223'};
+var personOne = {firstName: "John", lastName: "Johnson", phoneNumber: '404-333-3232'};
+var personTwo = {firstName: "Sally", lastName: "Johnson", phoneNumber: '404-334-2323'};
+var personThree = {firstName: "Sue", lastName: "Johnson", phoneNumber: '404-335-3223'};
+var $list = $('#newTable')
 
 var people = [personOne, personTwo, personThree];
+
+people.forEach(function(next){
+	$list.append('<tr>\
+					<td>' +next.firstName+ '\
+					</td>\
+					<td>' +next.lastName+ '\
+					</td>\
+					<td>' +next.phoneNumber+ '\
+					</td>');
+})
+
