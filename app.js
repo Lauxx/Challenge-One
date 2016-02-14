8c08b7ad5a8b8d8629025d0235998d7edc092c3f
Lauxx <lm.nichols@hotmail.com>
2016-02-14 13:08:21 -0700

    changed color of jumbotron, added jquery to hide submit button upon enter

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
+
+
 var personOne = {firstName: "John", lastName: "Johnson", phoneNumber: '404-333-3232'};
 var personTwo = {firstName: "Sally", lastName: "Johnson", phoneNumber: '404-334-2323'};
 var personThree = {firstName: "Sue", lastName: "Johnson", phoneNumber: '404-335-3223'};
-var $list = $('#newTable')
+var $list = $('#newEntry')
 
 var people = [personOne, personTwo, personThree];
 
 people.forEach(function(next){
-	$list.append('<tr>\
-					<td>' +next.firstName+ '\
-					</td>\
-					<td>' +next.lastName+ '\
-					</td>\
-					<td>' +next.phoneNumber+ '\
-					</td>');
-})
+
+$list.append(
+               '<tr> \
+                 <td>' + next.firstName + '</td> \
+                 <td>' + next.lastName + '</td> \
+                 <td>' + next.phoneNumber + '</td> \
+               </tr>');
+
+
+});
+
+
+ var addPersonToList = function(event){
+
+ 	event.preventDefault();
+ 	
+
+ 	var newFirstName = $('#newEntryFirstName').val();
+ 	var newLastName = $('#newEntryLastName').val();
+ 	var newPhoneNumber=$('#newEntryPhoneNumber').val();
+
+ 	confirm(' ' +newFirstName+ ' ' +newLastName+ ' ' +newPhoneNumber+ ' . Is this correct?');
+
+	$list.append(
+               '<tr> \
+                 <td>' + newFirstName + '</td> \
+                 <td>' + newLastName + '</td> \
+                 <td>' + newPhoneNumber + '</td> \
+               </tr>');
+ }
+
+ $('#submitButton').on('click', addPersonToList);
+
+$(document).ready(function(){
+  $('button').on('click', function(){
+    $('button').hide();
+  })
+});
+
 
